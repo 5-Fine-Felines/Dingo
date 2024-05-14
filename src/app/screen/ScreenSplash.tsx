8e975f3689b0ab2eca3c593cb3153ca93a2a2672
Suneth Udayanga <sunethuw@gmail.com>
2024-05-14 14:45:26 +0530

    add main function

---
+++ src/app/screen/ScreenSplash.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ScreenSplash = () => {
+  return (
+    <View>
+      <Text>ScreenSplash</Text>
+    </View>
+  )
+}
+
+export default ScreenSplash
