d9ed7c238b5194c7f191c9f2627f50cbd0a271a1
rashmika3433 <65844409+rashmika3433@users.noreply.github.com>
2024-05-26 17:18:11 +0530

    Splash screen rout set

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import Main from './src/Main';
+import ScreenSplash from './src/app/screen/ScreenSplash'; 
 
-function App(): React.JSX.Element {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); //delay time
+  }, []);
+
+  if (loading) {
+    return <ScreenSplash />;
+  }
+
   return (
-    <>
+    <View style={styles.container}>
       <Main />
-    </>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
